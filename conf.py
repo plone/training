a3c34557efee951e60224113e96a4ff918d28daa
Philip Bauer <bauer@starzel.de>
2014-05-09 15:57:11 +0200

    cleanup

--- conf.py
+++ conf.py
@@ -94,7 +94,6 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'default'
-#html_theme = "armstrong"
 html_theme = "sphinx_rtd_theme"
 
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
