fc8c723a1dc7e36ac65e4485284b478bd732106c
Philip Bauer <bauer@starzel.de>
2014-08-27 20:44:38 +0200

    use rtd-theme if not on rtd

--- conf.py
+++ conf.py
@@ -12,7 +12,10 @@
 # serve to show the default.
 
 import sys, os
-# import sphinx_rtd_theme
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:
+    import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -103,8 +106,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
-#html_theme = "sphinx_rtd_theme"
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 
 #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 #html_theme_path = ["_themes", ]
