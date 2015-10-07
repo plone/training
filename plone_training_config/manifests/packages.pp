f0d2be4fb2e7602f203c562f835a20e0868f28da
Johannes Raggam <raggam-nl@adm.at>
2015-10-07 23:36:39 +0200

    reformat, sort

--- plone_training_config/manifests/packages.pp
+++ plone_training_config/manifests/packages.pp
@@ -1,77 +1,31 @@
 class packages {
 
-  package { "build-essential":
-      ensure => present,
-  }
-  package { "python-dev":
-      ensure => present,
-  }
-  package { "libjpeg-dev":
-      ensure => present,
-  }
-  package { "libxml2-dev":
-      ensure => present,
-  }
-  package { "libxslt-dev":
-      ensure => present,
-  }
-  package { "git":
-      ensure => present,
-  }
-  package { "libz-dev":
-      ensure => present,
-  }
-  package { "libssl-dev":
-      ensure => present,
-  }
-  package { "subversion":
-      ensure => present,
-  }
-  package { "wget":
-      ensure => present,
-  }
-  package { "curl":
-      ensure => present,
-  }
-  package { "elinks":
-      ensure => present,
-  }
-  package { "vim":
-      ensure => present,
-  }
-  package { "gettext":
-      ensure => present,
-  }
-  package { "python-virtualenv":
-      ensure => present,
-  }
-  package { "unzip":
-      ensure => present,
-  }
-  package { "libpcre3-dev":
-      ensure => present,
-  }
-  package { "libedit-dev":
-      ensure => present,
-  }
-  package { "python-tk":
-      ensure => present,
-  }
-  package { "libyaml-dev":
-      ensure => present,
-  }
+  package { "build-essential":   ensure => present, }
+  package { "curl":              ensure => present, }
+  package { "elinks":            ensure => present, }
+  package { "gettext":           ensure => present, }
+  package { "git":               ensure => present, }
+  package { "libedit-dev":       ensure => present, }
+  package { "libjpeg-dev":       ensure => present, }
+  package { "libpcre3-dev":      ensure => present, }
+  package { "libssl-dev":        ensure => present, }
+  package { "libxml2-dev":       ensure => present, }
+  package { "libxslt-dev":       ensure => present, }
+  package { "libyaml-dev":       ensure => present, }
+  package { "libz-dev":          ensure => present, }
+  package { "python-dev":        ensure => present, }
+  package { "python-tk":         ensure => present, }
+  package { "python-virtualenv": ensure => present, }
+  package { "subversion":        ensure => present, }
+  package { "unzip":             ensure => present, }
+  package { "vim":               ensure => present, }
+  package { "wget":              ensure => present, }
   # Optional packages to enable indexing of office/pdf docs
-  # package { "wv":
-  #   ensure => present,
-  # }
-  # package { "poppler-utils":
-  #   ensure => present,
-  # }
+  # package { "wv":                ensure => present, }
+  # package { "poppler-utils":     ensure => present, }
 
   # used for creating a PuTTy-compatible key file
-  package { "putty-tools":
-    ensure => present,
-  }
+  package { "putty-tools":       ensure => present, }
 
 }
 
