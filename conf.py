854cc29db9284be059f5dc162158ef172d4676aa
Philip Bauer <bauer@starzel.de>
2016-10-23 17:29:54 -0400

    try to fix linkcheck_ignore

--- conf.py
+++ conf.py
@@ -50,7 +50,7 @@ linkcheck_ignore = [
     r'http://localhost:8080\d+/',
     r'http://localhost:8080',
     r'http://127.0.0.1:8080',
-    r'https://wiki.apache.org/solr',
+    r'http://wiki.apache.org',
 ]
 linkcheck_anchors = False
 linkcheck_timeout = 30.0
