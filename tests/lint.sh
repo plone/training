84fcdecc4beab10056d050fd8a4c26d7de399882
sven <sven@so36.net>
2017-08-16 11:25:04 +0200

    WIP: Add tests

---
+++ tests/lint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+
+CHANGED_FILES=$(git diff-index --name-only HEAD --cached | grep \.rst$)
+
+if [ -n "$CHANGED_FILES" ]; then
+	while read -r fname; do
+	    echo Testing: "$fname"
+	    docker run -v "${PWD}"/"$fname":/srv/docs:rw -v "${PWD}"/.ttd-lintrc:/srv/.ttd-lintrc testthedocs/plone-lint
+	done <<< "$CHANGED_FILES"
+    exit 0
+fi
+exit 0
+# Check if last commit includes a rst file
+#git diff-index --name-only HEAD --cached | grep \.rst$
