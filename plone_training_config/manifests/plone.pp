ebf86a5a48d1c61760a9ec231b42030f6e08ca51
Thomas Schorr <t_schorr@gmx.de>
2017-09-21 09:16:11 +0200

    Update Vagrant Build to use Ubuntu 16.04 LTS (#188)
    * use bento/ubuntu-16.04 box
    
    * resuming work on xenial update
    
    * Update CHANGES.txt
    
    * use vagrant_xenial_update branch of buildout

--- plone_training_config/manifests/plone.pp
+++ plone_training_config/manifests/plone.pp
@@ -1,28 +1,28 @@
 class plone {
 
-    $plone_version = "5.0.6"
+    $plone_version = "5.0.7"
 
-    file { ['/home/vagrant/tmp',
-            '/home/vagrant/.buildout',
-            '/home/vagrant/buildout-cache',
-            '/home/vagrant/buildout-cache/eggs',
-            '/home/vagrant/buildout-cache/downloads',
-            '/home/vagrant/buildout-cache/extends',
+    file { ['/home/ubuntu/tmp',
+            '/home/ubuntu/.buildout',
+            '/home/ubuntu/buildout-cache',
+            '/home/ubuntu/buildout-cache/eggs',
+            '/home/ubuntu/buildout-cache/downloads',
+            '/home/ubuntu/buildout-cache/extends',
             ]:
         ensure => directory,
-        owner => 'vagrant',
-        group => 'vagrant',
+        owner => 'ubuntu',
+        group => 'ubuntu',
         mode => '0755',
     }
 
-    file { '/home/vagrant/.buildout/default.cfg':
+    file { '/home/ubuntu/.buildout/default.cfg':
         ensure => present,
         content => inline_template('[buildout]
-eggs-directory = /home/vagrant/buildout-cache/eggs
-download-cache = /home/vagrant/buildout-cache/downloads
-extends-cache = /home/vagrant/buildout-cache/extends'),
-        owner => 'vagrant',
-        group => 'vagrant',
+eggs-directory = /home/ubuntu/buildout-cache/eggs
+download-cache = /home/ubuntu/buildout-cache/downloads
+extends-cache = /home/ubuntu/buildout-cache/extends'),
+        owner => 'ubuntu',
+        group => 'ubuntu',
         mode => '0664',
     }
 
@@ -40,59 +40,59 @@ extends-cache = /home/vagrant/buildout-cache/extends'),
     # Create virtualenv
     exec {'virtualenv --no-site-packages py27':
         alias => "virtualenv",
-        creates => '/home/vagrant/py27',
-        user => 'vagrant',
-        cwd => '/home/vagrant',
-        before => Exec["download_buildout_cache"],
+        creates => '/home/ubuntu/py27',
+        user => 'ubuntu',
+        cwd => '/home/ubuntu',
+        before => Exec["install_buildout_setuptools"],
         timeout => 300,
     }
 
-    # Download the buildout-cache from dist.plone.org
-    exec {"wget http://dist.plone.org/release/${plone_version}/buildout-cache.tar.bz2":
-        alias => "download_buildout_cache",
-        creates => "/home/vagrant/buildout-cache.tar.bz2",
-        cwd => '/home/vagrant',
-        user => 'vagrant',
-        group => 'vagrant',
-        before => Exec["unpack_buildout_cache"],
-        timeout => 600,
-    }
-
-    # Unpack the buildout-cache to /home/vagrant/buildout-cache/
-    exec {"tar xjf /home/vagrant/buildout-cache.tar.bz2":
-        alias => "unpack_buildout_cache",
-        creates => "/home/vagrant/buildout-cache/eggs/Products.CMFPlone-${plone_version}-py2.7.egg/",
-        user => 'vagrant',
-        cwd => '/home/vagrant',
+    # Install zc.buildout, setuptools
+    exec {'/home/ubuntu/py27/bin/pip install -U zc.buildout==2.5.3 setuptools==26.1.1':
+        alias => "install_buildout_setuptools",
+        creates => '/home/ubuntu/py27/bin/buildout',
+        user => 'ubuntu',
+        cwd => '/home/ubuntu',
         before => Exec["checkout_training"],
         timeout => 0,
     }
 
-    # get training buildout
-    exec {'git clone https://github.com/collective/training_buildout.git buildout && cd buildout && cd ..':
+    ## Download the buildout-cache from dist.plone.org - XXX training_buildout now uses 5.0.7 but there is no buildout cache dl for this release
+    #exec {"wget http://dist.plone.org/release/${plone_version}/buildout-cache.tar.bz2":
+    #    alias => "download_buildout_cache",
+    #    creates => "/home/ubuntu/buildout-cache.tar.bz2",
+    #    cwd => '/home/ubuntu',
+    #    user => 'ubuntu',
+    #    group => 'ubuntu',
+    #    before => Exec["unpack_buildout_cache"],
+    #    timeout => 600,
+    #}
+
+    ## Unpack the buildout-cache to /home/ubuntu/buildout-cache/
+    #exec {"tar xjf /home/ubuntu/buildout-cache.tar.bz2":
+    #    alias => "unpack_buildout_cache",
+    #    creates => "/home/ubuntu/buildout-cache/eggs/Products.CMFPlone-${plone_version}-py2.7.egg/",
+    #    user => 'ubuntu',
+    #    cwd => '/home/ubuntu',
+    #    before => Exec["checkout_training"],
+    #    timeout => 0,
+    #}
+
+    # get training buildout, xenial branch
+    exec {'git clone https://github.com/collective/training_buildout.git buildout && cd buildout && git checkout vagrant_xenial_update && cd ..':
         alias => "checkout_training",
         creates => '/vagrant/buildout',
-        user => 'vagrant',
+        user => 'ubuntu',
         cwd => '/vagrant',
-        before => Exec["bootstrap_training"],
-        timeout => 0,
-    }
-
-    # bootstrap training buildout
-    exec {'/home/vagrant/py27/bin/python bootstrap.py':
-        alias => "bootstrap_training",
-        creates => '/vagrant/buildout/bin/buildout',
-        user => 'vagrant',
-        cwd => '/vagrant/buildout',
         before => Exec["buildout_training"],
         timeout => 0,
     }
 
     # run training buildout
-    exec {'/vagrant/buildout/bin/buildout -c vagrant_provisioning.cfg':
+    exec {'/home/ubuntu/py27/bin/buildout -c vagrant_provisioning.cfg':
         alias => "buildout_training",
         creates => '/vagrant/buildout/bin/instance',
-        user => 'vagrant',
+        user => 'ubuntu',
         cwd => '/vagrant/buildout',
         # before => Exec["buildout_final"],
         timeout => 0,
