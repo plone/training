95237798bad84a865e0f1d49a23655b0d9059138
Érico Andrei <ericof@gmail.com>
2019-08-18 21:10:20 +0200

    wip: auto deployment

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ then
   eval "$(ssh-agent)"
   chmod 600 $HOME/.ssh/id_rsa
   ssh-add $HOME/.ssh/id_rsa
-  rsync -avz --delete -e ''$ssh_connection'' _build/html/ $docs_server:/var/www/training.plone.org/5
+  rsync -avz --delete -e 'ssh -i ~/.ssh/id_rsa -p 6822' _build/html/ $docs_server:/var/www/training.plone.org/5
   echo 'Website published successfully.'
 else
   echo "Build successful, but not publishing!"
