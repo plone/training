8ccbdda3d19950b65447348867011e1ad74d6bb3
Érico Andrei <ericof@gmail.com>
2019-08-18 19:31:31 +0200

    Use travis environment variables for the connection

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ then
   eval "$(ssh-agent)"
   chmod 600 $HOME/.ssh/id_rsa
   ssh-add $HOME/.ssh/id_rsa
-  rsync -avz --delete -e 'ssh -i /home/travis/.ssh/id_rsa' _build/html/ docs-update@docs.plone.org:/var/www/training.plone.org/5
+  rsync -avz --delete -e '$ssh_connection' _build/html/ $docs_server:/var/www/training.plone.org/5
   echo 'Website published successfully.'
 else
   echo "Build successful, but not publishing!"
