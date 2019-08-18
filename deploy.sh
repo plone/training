2a295eb66c3b5516c66b97003154db72ed46b984
Érico Andrei <ericof@gmail.com>
2019-08-18 21:49:35 +0200

    Lint deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if ([ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ])
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
   eval "$(ssh-agent)"
-  chmod 600 $HOME/.ssh/id_rsa
-  ssh-add $HOME/.ssh/id_rsa
-  rsync -avz --delete -e 'ssh -i ~/.ssh/id_rsa -p 6822 -o "StrictHostKeyChecking=no"' _build/html/ $docs_server:/var/www/training.plone.org/5
+  chmod 600 "$HOME/.ssh/id_rsa"
+  ssh-add "$HOME/.ssh/id_rsa"
+  rsync -avz --delete -e 'ssh -i ~/.ssh/id_rsa -p 6822 -o "StrictHostKeyChecking=no"' _build/html/ "$DOCS_SERVER:/var/www/training.plone.org/5"
   echo 'Website published successfully.'
 else
   echo "Build successful, but not publishing!"
