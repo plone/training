fd572facf1a6fc02827ed44992b4eadd4fbf94b2
Katja Suess <k.suess@rohberg.ch>
2020-05-05 21:18:35 +0200

    watch changes with gulp
    browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+/**
+ * Install with 
+ * npm install
+ * 
+ * Run with
+ * gulp
+ * to watch changes reflected on http://localhost:3002/
+ */
+
+const gulp = require("gulp");
+const browserSync = require('browser-sync').create();
+var exec = require('child_process').exec;
+
+function makeHtml() {
+    const cmd = 'make html';
+    return new Promise((resolve, reject) => {
+        exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+                console.error(error);
+                reject(error);
+            }
+            resolve(stdout? stdout : stderr);
+        });
+    });
+}
+
+function buildAndBrowserSync(done) {
+    makeHtml().then(function() {
+        browserSync.reload();
+    }, function(Error) {
+        console.log(Error);
+    });
+    done();
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './_build/html/'
+        }
+    });
+    gulp.watch(['./**/*.rst',], buildAndBrowserSync);
+}
+
+exports.default = watch;
