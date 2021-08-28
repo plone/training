8286b4a12b7e304bd4ced3e122cd36d432fed89d
ksuess <k.suess@rohberg.ch>
2021-08-28 19:25:38 +0200

    configuration: extensions of sphinx and theme 'book'

--- conf.py
+++ conf.py
@@ -28,6 +28,7 @@ author = 'Plone Community'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "myst_parser",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -36,7 +37,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'node_modules']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -44,9 +45,20 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_book_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+    "repository_url": "https://github.com/plone/training",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+}
+myst_enable_extensions = [
+    'deflist',
+    'linkify'
+]
