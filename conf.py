d5ab4d11fdf6418275fea7973e4dcac51e5a2b6e
ksuess <k.suess@rohberg.ch>
2021-08-29 15:28:04 +0200

    Link to plone.org in left sidebar

--- conf.py
+++ conf.py
@@ -61,7 +61,7 @@ html_theme_options = {
     "use_repository_button": True,
     "use_issues_button": True,
     "use_edit_page_button": True,
-    "extra_navbar": "<p><a href='https://plone.org'>Plone.org</a></p>",
+    "extra_navbar": "<p class='ploneorglink'><a href='https://plone.org'><img src='/_static/logo.svg' alt='plone.org' /> plone.org</a></p>",
 }
 myst_enable_extensions = [
     'deflist',
