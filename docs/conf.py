0d7e650b7b06f636e6f835d52f1f3a03c4bcdd5d
Steve Piercy <web@stevepiercy.com>
2022-05-05 06:07:52 -0700

    Enable Google Analytics 4

--- docs/conf.py
+++ docs/conf.py
@@ -174,6 +174,7 @@ html_extra_path = [
 html_static_path = ["_static"]
 
 html_theme_options = {
+    "google_analytics_id": "G-P8NCTB796E",
     "repository_url": "https://github.com/plone/training",
     "repository_branch": "main",
     "path_to_docs": "docs",
