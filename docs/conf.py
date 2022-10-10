e9b9edc89ed274151792c21364c7d87662c1cc60
Érico Andrei <ericof@gmail.com>
2022-10-10 09:00:28 +0200

    Update list of links to be ignored

--- docs/conf.py
+++ docs/conf.py
@@ -102,9 +102,9 @@ linkcheck_ignore = [
     r"https://plonedemo.kitconcept.com",  # Did Not Connect: Potential Security Issue
     r"https://www.packtpub.com/.*",  # test say 500 Server Error but manually they work
     r"https://www.dipf.de/.*",  # a timeout from time to time
-    r"http://plone-conference.localhost/.*",
-    r"http://plone-conference.localhost:3000/.*",
-    r"https://plone-conference.localhost/.*",
+    r"http://plone-conference.localhost.*",
+    r"http://plone-conference.localhost:3000.*",
+    r"https://plone-conference.localhost.*",
     # ### Start of list of anchored links
     # Prior to each PloneConf, uncomment these lines to verify that the links work,
     # although the anchor cannot be found.
