4cde109ac539d2133ca7f6a7f5286760a032bdc0
Katja Süss <k.suess@rohberg.ch>
2023-09-05 15:46:57 +0200

    Bug fix scrolling on navigation: current nav item AND article title on top

--- docs/_static/patch_scrollToActive.js
+++ docs/_static/patch_scrollToActive.js
@@ -20,6 +20,9 @@ var sbRunWhenDOMLoaded = (cb) => {
     let active_navigation_item = [...document.querySelectorAll("li.current.active")].pop();
     if (active_navigation_item) {
       active_navigation_item.scrollIntoView();
+
+      let article = [...document.querySelectorAll("article")].pop();
+      article.scrollIntoView({inline: "start" });
     }
   };  
   
